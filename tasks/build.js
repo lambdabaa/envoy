75c8c6c43ec755443394a6c2a303bdf8be0c49e6
gaye <gaye@mozilla.com>
2014-04-16 16:14:28 -0400

    Use project scoped copy of mrt

--- tasks/build.js
+++ tasks/build.js
@@ -5,7 +5,10 @@ module.exports = function(grunt) {
     // 1. Copy the source tree into a temporary, build directory.
     execf('cp -r %s %s', __dirname + '/../app/', __dirname + '/../build/');
     // 2. Run |mrt install|.
-    execf('cd %s && mrt install', __dirname + '/../build/');
+    execf(
+      'cd %s && ../node_modules/.bin/mrt install',
+      __dirname + '/../build/'
+    );
     // TODO(gareth): Bundle the meteor app and push the app tarball to
     //     a docker container.
   });
